4f2922d9a2acdb9650688e16b1090ad65ed2d1ae
SauravNaruka <saurav.singh.n@gmail.com>
2024-05-05 14:28:57 +0530

    Add initial project config and setup

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ThemeToggle from "../components/ThemeToggle";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ThemeToggle />
+    </main>
+  );
+}
